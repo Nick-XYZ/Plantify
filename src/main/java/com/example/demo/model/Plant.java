1279ca6002c81bf281f2a6a3b909a26f8b5b5364
Nick-XYZ <123240062+Nick-XYZ@users.noreply.github.com>
2023-04-03 16:35:59 +0200

    småfix av gamla merge.

--- src/main/java/com/example/demo/model/Plant.java
+++ src/main/java/com/example/demo/model/Plant.java
@@ -32,7 +32,6 @@ public class Plant {
 
     public String getPlantStageImg() {
         LocalDate now = LocalDate.now();
-//ready to eat delat på 5 in i en phase som sedan används nedan.
         Long dif = ChronoUnit.DAYS.between(this.getCreated(), now);
         if (dif < 10) {
             // /1/images/t1.png
@@ -47,4 +46,24 @@ public class Plant {
             return "/images/" + this.species.getId().toString() + "/t5.png";
         }
     }
+
+   /* public String getPlantStageImgResponsive() {
+        LocalDate now = LocalDate.now();
+//ready to eat delat på 5 in i en phase som sedan används nedan.
+        Long dif = ChronoUnit.DAYS.between(this.getCreated(), now);
+        Long readyToEat = species.getReadyToEat();
+        Long phase=
+        if (dif < 10) {
+            // /1/images/t1.png
+            return "/images/" + this.species.getId().toString() + "/t1.png";
+        } else if (dif > 10 && dif < 18) {
+            return "/images/" + this.species.getId().toString() + "/t2.png";
+        } else if (dif > 18 && dif < 26) {
+            return "/images/" + this.species.getId().toString() + "/t3.png";
+        } else if (dif > 26 && dif < 32) {
+            return "/images/" + this.species.getId().toString() + "/t4.png";
+        } else {
+            return "/images/" + this.species.getId().toString() + "/t5.png";
+        }
+    }*/
 }
