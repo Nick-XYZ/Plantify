b64c0bb87e7cfe25795a4fdcbb1add5ad5534557
Michelle Steenvoorden <michellesteenvoorden93@gmail.com>
2023-04-06 09:22:50 +0200

    merge fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,8 +10,9 @@
     ('Cherry Tomatoes', 'Solanum lycopersicum var. cerasiforme', 'Direct sunlight', 'Vegetable soil', 1, 30, 7, 120, 'A bountiful, compact cherry tomato that does not need to be plucked or tied up. Produces sweet little tomatoes all summer long. Perfect for the balcony, terrace or in the kitchen window. Grow indoors.'),
     ('Strawberries', 'Fragaria', 'Direct sunlight', 'Vegetable soil with cow manure', 14, 30, 14, 120, 'Strawberries can get planted early in the spring and can be harvested between June and August.'),
     ('Sweetpeas','Pisum sativum','Direct sunlight','soil', 2,30, 5,60,'Sweetpeas are easy to handle.You should plant it with some weeks apart,because the plant have a shortlife and this extends the season.The more you harvest,the more the plant produces.'),
-    ('Chili','Capsicum annuum','Direct sunlight', 'soil', 1,20,3,186,'Chili can be planted all year around,but needs extra light in the winter. Chili can be pre-plant in the end of march and needs to be repotted. It loves warm places and sunlight and needs protection from heavywind.'),
-    ('Carrot','Daucus','Moderate sunlight', 'soil', 1,240, 30 ,100,'These type of carrots get planted in the spring and can be harvested continuously under the summer months.');
+        ('Carrot','Daucus','Moderate sunlight', 'soil', 1,240, 30 ,100,'These type of carrots get planted in the spring and can be harvested continuously under the summer months.'),
+    ('Chili','Capsicum annuum','Direct sunlight', 'soil', 1,20,3,186,'Chili can be planted all year around,but needs extra light in the winter. Chili can be pre-plant in the end of march and needs to be repotted. It loves warm places and sunlight and needs protection from heavywind.');
+
 
 
     INSERT INTO PLANT (PLANT_NAME, CREATED, SPECIES_ID, ADMIN_ID) VALUES
